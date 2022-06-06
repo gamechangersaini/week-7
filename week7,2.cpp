90bdf59d8ffacf3328f81875410e46d14ab76958
Anmol Saini <103759375+gamechangersaini@users.noreply.github.com>
2022-06-06 23:25:06 +0530

    Add files via upload

---
+++ week7,2.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void calulate(vector<int> &pa, int i) {
+    cout << i + 1 << " ";
+    if (pa[i] >= 0)
+        calulate(pa, pa[i]);
+}
+
+void find_path(int **graph, int m, int sour) {
+    vector<int> dis(m, INT_MAX), pa(m, -1);
+    dis[sour] = 0;
+    for (int ki = 0; ki < m - 1; ki++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (graph[i][j] != 0) {
+                    if (dis[j] > dis[i] + graph[i][j]) {
+                        dis[j] = dis[i] + graph[i][j];
+                        pa[j] = i;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        calulate(pa, i);
+        cout << ": " << dis[i] << endl;
+    }
+}
+
+int main() {
+    int m, source, ed;
+    cin >> m;
+    int **graph = (int **)malloc(m * sizeof(int *));
+    for (int i = 0; i < m; i++)
+        graph[i] = (int *)malloc(m * sizeof(int));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> graph[i][j];
+        }
+    }
+    cin >> source;
+    find_path(graph, m, source - 1);
+}
